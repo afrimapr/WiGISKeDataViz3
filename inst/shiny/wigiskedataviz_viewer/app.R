f30c747ee10ca4a19b88c997ee87134f472a9a3b
Anelda van der Walt <anelda.vdwalt@gmail.com>
2020-08-13 08:17:38 +0200

    Added info about maps in Shiny

--- inst/shiny/wigiskedataviz_viewer/app.R
+++ inst/shiny/wigiskedataviz_viewer/app.R
@@ -24,8 +24,9 @@ ui <- shinydashboard::dashboardPage(skin = "black",
                                 box(leaflet::leafletOutput("map_pregs_normalised2017"), width = 4, title = "2017"),
                                 box(leaflet::leafletOutput("map_pregs_normalised2018"), width = 4, title = "2018"),
                                 box(leaflet::leafletOutput("map_pregs_normalised2019"), width = 4, title = "2019"),
-                                box(leaflet::leafletOutput("map_pregs_normalised2020"), width = 4, title = "2020")
-                            )
+                                box(leaflet::leafletOutput("map_pregs_normalised2020"), width = 4, title = "2020"),
+                                box(shiny::textOutput("map_pregs_normalised_info"), width = 4, title = "Understanding the map and underlying data")
+                                )
                         ),
 
                 # Second tab content
@@ -59,6 +60,21 @@ server <- function(input, output, session) {
 
     output$map_pregs_normalised2020 <- leaflet::renderLeaflet({
         tmap::tmap_leaflet(map_norm_preg(data_sf = ken_preg_sf, age_group = input$age, year = 2020))
+    })
+
+    output$map_pregs_normalised_info = renderText({
+        paste0("Data shown here represents a cleaned up version of the data received from WiGISKE for the
+               third data visualisation challenge. The scripts showing how the data was cleaned up is available
+               in our Github repository.
+
+               Raw numbers were normalised using population numbers for the different age
+               groups obtained from the World Bank Data Bank for the years 2016 - 2020. As we only received pregnancy
+               data for the first 2 quarters of 2020, we simply multiplied those numbers by two to be able to compare
+               against other full years. We recognise that there are more elegant ways of modeling the expected number
+               of pregnancies for the year 2020.
+
+               The data was grouped by County although numbers are also available for sub counties and wards and is
+               available for further exploration.")
     })
 }
 
